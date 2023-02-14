e582baa4bd20b6b81667b714ac0488566ecd080f
HarshMalviya2023 <123155805+HarshMalviya2023@users.noreply.github.com>
2023-02-14 16:08:26 +0530

    Add files via upload

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,3 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.examples;
+
+// beam-playground:
+//   name: WordCount
+//   description: An example that counts words in Shakespeare's works.
+//   multifile: false
+//   pipeline_options: --output output.txt
+//   context_line: 95
+//   categories:
+//     - Combiners
+//     - Options
+//     - Quickstart
+
 import org.apache.beam.examples.common.ExampleUtils;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
@@ -171,5 +201,5 @@ public class WordCount {
         PipelineOptionsFactory.fromArgs(args).withValidation().as(WordCountOptions.class);
 
     runWordCount(options);
-  }
+  }
 }
